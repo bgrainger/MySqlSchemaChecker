b07efff2df34223e0523f4d4141856cc196ea482
Bradley Grainger <bgrainger@gmail.com>
2017-10-21 19:53:16 -0700

    Add empty console application.

---
+++ MySqlSchemaChecker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySqlSchemaChecker
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
